55e7674cb808143434d64bead3cd719e887386b4
Felipe Duarte <feduarte.dev@gmail.com>
2024-04-05 11:08:36 -0300

    requisito 4 fixed

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -15,9 +15,22 @@ class Importer(ABC):
 
 class JsonImporter(Importer):
     def import_data(self) -> list[Product]:
+        result = []
         with open(self.path) as file:
             data = json.load(file)
-        return data
+            for product in data:
+                product_obj = Product(
+                    product["id"],
+                    product["product_name"],
+                    product["company_name"],
+                    product["manufacturing_date"],
+                    product["expiration_date"],
+                    product["serial_number"],
+                    product["storage_instructions"],
+                )
+                result.append(product_obj)
+
+        return result
 
 
 class CsvImporter:
@@ -30,3 +43,8 @@ IMPORTERS: Dict[str, Type[Importer]] = {
     "json": JsonImporter,
     "csv": CsvImporter,
 }
+
+JSON_DATA = "inventory_report/data/inventory.json"
+
+teste = JsonImporter(JSON_DATA)
+print(teste.import_data())
--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,10 @@
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: list[Product] = []) -> None:
+        self.data = data
+
+    def add_data(self, products_list: list[Product]):
+        for product in products_list:
+            self.data.append(product)
